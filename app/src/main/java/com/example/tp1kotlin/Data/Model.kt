e530d16db3ad4f367848a2bedc96cbe397bed897
Erij-ameur <erij.ameur02@gmail.com>
2024-11-28 09:28:32 +0100

    modèle ok

--- app/src/main/java/com/example/tp1kotlin/Data/Model.kt
+++ app/src/main/java/com/example/tp1kotlin/Data/Model.kt
@@ -100,3 +100,53 @@ data class Actor(
     val profile_path: String,
     val isFav: Boolean = false
 )
+
+
+data class Playlist(
+    val checksum: String,
+    val collaborative: Boolean,
+    val cover: String,
+    val creation_date: String,
+    val creator: Creator,
+    val description: String,
+    val duration: Int,
+    val fans: Int,
+    val id: Int,
+    val is_loved_track: Boolean,
+    val link: String,
+    val md5_image: String,
+    val nb_tracks: Int,
+    val picture_type: String,
+    val `public`: Boolean,
+    val share: String,
+    val title: String,
+    val tracklist: String,
+    val tracks: Tracks,
+    val type: String
+)
+
+data class Creator(
+    val id: Int,
+    val name: String,
+    val tracklist: String,
+    val type: String
+)
+
+data class Artist(
+    val id: Int,
+    val link: String,
+    val name: String,
+    val tracklist: String,
+    val type: String
+)
+
+
+data class Album(
+    val cover: String,
+    val id: Int,
+    val md5_image: String,
+    val title: String,
+    val tracklist: String,
+    val type: String,
+    val upc: String
+)
---
+++ app/src/main/java/com/example/tp1kotlin/Data/Tracks.kt
@@ -0,0 +1,6 @@
+package com.example.tp1kotlin.Data
+
+data class Tracks(
+    val checksum: String,
+    val `data`: List<Data>
+)
